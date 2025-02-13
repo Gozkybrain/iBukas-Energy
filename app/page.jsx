ea439c1d6b57447094887c52412aa6a53c4cfbe0
Gee Brain <gozkybrain@gmail.com>
2025-02-13 17:05:58 +0100

    Hero Section has Animation

--- app/page.jsx
+++ app/page.jsx
@@ -11,15 +11,16 @@ export default function Home() {
       <div className="hero-section">
         <div className="overlay">
           <div className="content">
-            <h2 className="hero-head">Ready to take the Solar Step?</h2>
-            <div className="hero-text">
+            <h2 className="hero-head animate__animated animate__fadeInUp">
+              Ready to take the Solar Step?</h2>
+            <div className="hero-text animate__animated animate__fadeInUp animate__delay-1s">
               Our Solar Calculator gives you real-time estimates on how much solar can cut your electricity bills.
               No sales pitch, just clear numbers.
               Find out how much power your roof can generate today.
             </div>
 
             <button
-              className="hero-btn"
+              className="hero-btn animate__animated animate__fadeInUp animate__delay-2s"
               onClick={() => router.push("/calculator")}>
               Start Calculation
             </button>
