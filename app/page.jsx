9262b9eda558acb370c85acbaee8d605d561e41e
Gee Brain <gozkybrain@gmail.com>
2025-02-14 17:15:50 +0100

    Custom useInView hook introduced

--- app/page.jsx
+++ app/page.jsx
@@ -5,12 +5,15 @@ import Carousel from "./components/Carousel";
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBolt, faLeaf } from "@fortawesome/free-solid-svg-icons";
-import useInView from "../hooks/useInView";
+import useInView from "./hooks/useInView";
 
 export default function Home() {
   const router = useRouter();
 
   // Animations
+  const [heroRef, heroInView] = useInView();
+  const [heroTextRef, heroTextInView] = useInView();
+  const [btnRef, btnInView] = useInView();
   const [headingRef, headingInView] = useInView();
   const [textRef, textInView] = useInView();
   const [imageRef, imageInView] = useInView();
@@ -21,17 +24,22 @@ export default function Home() {
       <div className="hero-section">
         <div className="overlay">
           <div className="content">
-            <h2 className="hero-head animate__animated animate__fadeInUp">
+            <h2
+              ref={heroRef}
+              className={`hero-head animate__animated ${heroInView ? "animate__fadeInUp" : ""}`}>
               Ready to take the Solar Step?
             </h2>
-            <div className="hero-text animate__animated animate__fadeInUp animate__delay-1s">
+            <div
+              ref={heroTextRef}
+              className={`hero-text animate__animated ${heroTextInView ? "animate__fadeInUp animate__delay-1s" : ""}`}>
               Our Solar Calculator gives you real-time estimates on how much solar can cut your electricity bills.
               No sales pitch, just clear numbers.
               Find out how much power your roof can generate today.
             </div>
 
             <button
-              className="hero-btn animate__animated animate__fadeInUp animate__delay-2s"
+              ref={btnRef}
+              className={`hero-btn animate__animated ${btnInView ? "animate__fadeInUp animate__delay-2s" : ""}`}
               onClick={() => router.push("/calculator")}
             >
               Start Calculation
